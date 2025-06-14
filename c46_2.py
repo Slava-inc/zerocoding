39acd4b5cf94c6b428c36ff26b48467c81b69963
slavaincos <slavaincos@gmail.com>
2025-06-14 15:39:04 +0300

    inline, reply telegram buttons

---
+++ c46_2.py
@@ -0,0 +1,77 @@
+from my_token import TOKEN
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+
+def create_main_menu_keyboard():
+    keyboard = [
+        [InlineKeyboardButton("🌤 Показать погоду", callback_data='weather')],
+        [
+            InlineKeyboardButton("Информация", callback_data='info'),
+            InlineKeyboardButton("🖥 Перейти в магазин", url="https://zerocoder.ru")
+        ]
+
+    ]
+    return InlineKeyboardMarkup(keyboard) # Создаем разметку клавиатуры
+
+def start(update: Update, context: CallbackContext) -> None:
+    user = update.effective_user
+    update.message.reply_text(
+        f"Привет, {user.first_name}! Я твой бот-помощник.Что ты хочешь сделать?",
+        # reply_markup=create_main_menu_keyboard()
+        reply_markup = create_reply_keyboard()
+    )
+    update.message.reply_text(
+        "Используйте кнопки ниже или меню: ",
+        reply_markup=create_main_menu_keyboard()
+    )
+    
+
+def button_click(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query # Получаем информацию о нажатии
+    query.answer()  # Подтверждаем получение callback (убирает "часики" у кнопки)
+
+    if query.data == "weather":
+        back_button = [[InlineKeyboardButton("⬅️ Назад в меню", callback_data="back_to_menu")]]
+        reply_markup = InlineKeyboardMarkup(back_button)
+        query.edit_message_text(
+            text="Функция в разработке, попробуйте позже.", reply_markup=reply_markup
+        )
+    elif query.data == "info":
+        query.edit_message_text(
+            text="Это учебный бот")        
+    elif query.data == 'back_to_menu':
+        query.edit_message_text(
+            text="Выберите действие:",
+            reply_markup=create_main_menu_keyboard())
+    elif query.data == 'close':
+        query.delete_message()
+
+def create_reply_keyboard(): # клавиатура основного меню
+    return ReplyKeyboardMarkup(        
+         [
+            [ "Мой профиль"],
+            [KeyboardButton("Отправить контакт", request_contact=True, resize_keyboard=True)],
+         ],
+        resize_keyboard=True,
+        input_field_placeholder="Выберите действие"
+    )
+    
+def create_profile_keyboard():
+    return ReplyKeyboardMarkup(
+        [["✏️ Изменить имя", "Дата рождения", ["Главное меню"]]],
+        resize_keyboard=True,
+        one_time_keyboard=True,
+            )
+    
+    
+def main():  # Основная функция для настройки и запуска бота
+    updater = Updater(TOKEN) # Создаем объект для работы с API Telegram
+    dispatcher = updater.dispatcher # Получаем диспетчер для регистрации обработчиков
+    dispatcher.add_handler(CommandHandler("start", start)) # через /start
+    dispatcher.add_handler(CallbackQueryHandler(button_click)) # через кнопку
+    updater.start_polling() # Начинаем опрос сервера Telegram
+    updater.idle() # Бот работает до принудительной остановки
+
+if __name__ == '__main__':    
+    main()
--- doc_pdf.py
+++ doc_pdf.py
@@ -3,14 +3,14 @@ import os
 
 def images_to_pdf(image_files, output_pdf):
     # Открываем изображения и преобразуем их в формат PDF
-    images = [Image.open(image_file).convert('RGB') for image_file in image_files]
+    images = [Image.open(f'homework//{image_file}').convert('RGB') for image_file in image_files]
 
     # Сохраняем изображения в файл PDF
     if images:
         images[0].save(output_pdf, save_all=True, append_images=images[1:])
 
 # Пример использования
-image_files = ['C44_0.png', 'C44_1.png', 'C44_2.png', 'C44_3.png', 'C44_4.png', 'C44_5.png', 'C44_6.png', 'C44_7.png']  # Замените на ваши файлы
-output_pdf = 'C44.pdf'
+image_files = ['C46_1.png', 'C46_2.png', 'C46_3.png', 'C46_4.png', 'C46_5.png', 'C46_6.png', 'C46_7.png']  # Замените на ваши файлы
+output_pdf = 'C46.pdf'
 
 images_to_pdf(image_files, output_pdf)
